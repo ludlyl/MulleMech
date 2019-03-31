36ede636f7db7c77dc82ea4a8b85bed87b03d456
Kevin Solovjov <kevin.solovjov@gmail.com>
2019-03-31 03:20:42 +0200

    Hellions don't attack flying units

--- src/plugins/micro/Hellion.cpp
+++ src/plugins/micro/Hellion.cpp
@@ -1,5 +1,5 @@
 #include "Hellion.h"
-
+#include "core/Helpers.h"
 #include "core/API.h"
 
 Hellion::Hellion(Unit* unit)
@@ -11,8 +11,20 @@ void Hellion::OnCombatStep(const Units& enemies) {
     DefaultUnit::OnCombatStep(enemies);
 
     if (m_self->weapon_cooldown == 0) {
-        const Unit* target = enemies.GetClosestUnit(m_self->pos);
-        Cast( sc2::ABILITY_ID::SMART, target);
+
+        //Get rid of flying units
+        auto it = std::remove_if(enemies.ToAPI().begin(), enemies.ToAPI().end(), [](const Unit *unit_) {
+            return IsFlying()(*unit_);
+        });
+
+        enemies.ToAPI().erase(it, enemies.ToAPI().end());
+
+        if (!enemies.ToAPI().empty()) {
+        const Unit *target = enemies.GetClosestUnit(m_self->pos);
+
+        Cast(sc2::ABILITY_ID::SMART, target);
+    }
+
     } else {
         //TODO Smarter retreat pathing
         MoveTo(sc2::Point2D(gAPI->observer().StartingLocation().x,
