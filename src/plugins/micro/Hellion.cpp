15c2dc137c2efcc4c4e44d91894dc44a9facb5e7
Kevin Solovjov <kevin.solovjov@gmail.com>
2019-03-31 01:58:28 +0100

    Small optimization

--- src/plugins/micro/Hellion.cpp
+++ src/plugins/micro/Hellion.cpp
@@ -10,10 +10,9 @@ Hellion::Hellion(Unit* unit)
 void Hellion::OnCombatStep(const Units& enemies) {
     DefaultUnit::OnCombatStep(enemies);
 
-    const Unit* target = enemies.GetClosestUnit(m_self->pos);
-
     if (m_self->weapon_cooldown == 0) {
-            Cast( sc2::ABILITY_ID::SMART, target);
+        const Unit* target = enemies.GetClosestUnit(m_self->pos);
+        Cast( sc2::ABILITY_ID::SMART, target);
     } else {
         //TODO Smarter retreat pathing
         MoveTo(sc2::Point2D(gAPI->observer().StartingLocation().x,
