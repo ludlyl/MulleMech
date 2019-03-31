3fd46d7a29a52e7b97387466175786da05a6b11d
Kevin Solovjov <kevin.solovjov@gmail.com>
2019-03-31 19:11:54 +0200

    Distance fix

--- src/plugins/micro/Reaper.cpp
+++ src/plugins/micro/Reaper.cpp
@@ -14,7 +14,7 @@ void Reaper::OnCombatStep(const Units& enemies) {
     //Retreat
     if ((m_self->health) < (35) && !((gAPI->observer().StartingLocation().x == m_self->pos.x) &&
                                      (gAPI->observer().StartingLocation().y == m_self->pos.y))) {
-        if (DistanceSquared2D(target->pos, m_self->pos) < 5) {
+        if (Distance2D(target->pos, m_self->pos) < 5) {
             Cast(sc2::ABILITY_ID::EFFECT_KD8CHARGE, target);
         }
         MoveTo(sc2::Point2D(gAPI->observer().StartingLocation().x,
@@ -23,13 +23,13 @@ void Reaper::OnCombatStep(const Units& enemies) {
     } else {
         if (m_self->weapon_cooldown == 0) {
 
-            if (DistanceSquared2D(target->pos, m_self->pos) < 25) {
+            if (Distance2D(target->pos, m_self->pos) < 25) {
                 //Bombs
                 Cast(sc2::ABILITY_ID::EFFECT_KD8CHARGE, target);
                 Cast(sc2::ABILITY_ID::SMART, target);
             }
         } else {
-            if (DistanceSquared2D(target->pos, m_self->pos) < 5) {
+            if (Distance2D(target->pos, m_self->pos) < 5) {
                 Cast(sc2::ABILITY_ID::EFFECT_KD8CHARGE, target);
             }
             MoveTo(sc2::Point2D(gAPI->observer().StartingLocation().x,
