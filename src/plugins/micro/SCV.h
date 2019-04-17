f82db637e34382a265d4a84f4f9b017e522ad9e4
Nightingale1997 <kevin.solovjov@gmail.com>
2019-04-17 16:32:16 +0200

    isMechanical helper and SCV class

---
+++ src/plugins/micro/SCV.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "MicroPlugin.h"
+
+class SCV : public MicroPlugin {
+public:
+    explicit SCV(Unit* unit);
+
+    void OnCombatStep(const Units& enemies, const Units& allies) override;
+};
