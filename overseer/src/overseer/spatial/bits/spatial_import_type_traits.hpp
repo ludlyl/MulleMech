d2823615b8423684b445bf6f7d33e71ad7121db7
ludlyl <ludvig.lyl@gmail.com>
2019-04-24 14:09:50 +0200

    Fix spatial compilation errors for VC++

--- overseer/src/overseer/spatial/bits/spatial_import_type_traits.hpp
+++ overseer/src/overseer/spatial/bits/spatial_import_type_traits.hpp
@@ -9,11 +9,6 @@
  *  \file spatial_import_type_traits.hpp Contains the macro to pull certain
  *  "type_traits" dependancies into the spatial::import namespace.
  *
- *  Depending on the library support and compiler version, the type_traits
- *  headers can only be found in TR1 namespace (MSVC 2010 and glibc++ without
- *  c++11 support), only be found in STD name space (libc++), or both (MSVC2012
- *  and glibc++ with C++11 support). This file is written to import these types,
- *  regardless of the library being used, into the spatial::import namespace.
  */
 
 #ifndef SPATIAL_IMPORT_TYPE_TRAITS
@@ -23,19 +18,8 @@
 #  undef SPATIAL_TYPE_TRAITS_NAMESPACE
 #endif
 
-#if defined(__LIBCPP_VERSION) || __cplusplus >= 201103L
-#  include <type_traits>
-#  define SPATIAL_TYPE_TRAITS_NAMESPACE std
-#elif defined(__GLIBCXX__)
-#  include <tr1/type_traits>
-#  define SPATIAL_TYPE_TRAITS_NAMESPACE std::tr1
-#else
-#  ifdef __IBMCPP__
-#    define __IBMCPP_TR1__
-#  endif
-#  include <type_traits>
-#  define SPATIAL_TYPE_TRAITS_NAMESPACE std::tr1
-#endif
+#include <type_traits>
+#define SPATIAL_TYPE_TRAITS_NAMESPACE std
 
 namespace spatial
 {
