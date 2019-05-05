928192ae07d967de699666a1fd1470638b2cf7ac
Andreas <gusrudean@student.gu.se>
2019-05-05 18:56:16 +0200

    Add BuildOrderStages

--- src/plugins/Governor.h
+++ src/plugins/Governor.h
@@ -26,15 +26,23 @@ struct Governor : Plugin {
     // Same as above but with units (Units in this case does not include structures aka buildings)
     int CountTotalUnits(Builder* builder_, sc2::UNIT_TYPEID type);
     
-
 private:
+    enum class BuildOrderStage {Early, Mid, Finished};
+
+    void AddEarlyGameBuildOrder(Builder* builder_);
+
+    void AddMidGameBuildOrder(Builder* builder_);
+
     //Used the put all commandcenters in the planner queue at the top of the list
     void PrioritizeCommandCenter();
 
     std::list<sc2::UNIT_TYPEID> m_planner_queue;
+    BuildOrderStage m_build_order_stage = BuildOrderStage::Early;
 
     static constexpr float ThorsToTanksRatio = 1.f / 3.f;
     static constexpr int OptimalNumOfRavens = 2;
     static constexpr float MedivacsToHellbatsRatio = 1.f / 8.f;
     static constexpr float HellionProductionChance = 0.2f;
+    static constexpr int MineralsBufferThreshold = 500;     // If we got this much buffer, move build order stage Early->Mid
+    static constexpr int MaxMarines = 8;                    // Never exceed this count of marines
 };
